66ec045826170b9c1af9deda28023b5d5efe5bed
Kunal Dua <kunaldua2000@gmail.com>
2022-09-24 20:51:14 +0530

    view for list added

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,13 +1,63 @@
-import { SafeAreaView, ScrollView, StyleSheet, Text, View } from "react-native";
-import React from "react";
+import {
+  SafeAreaView,
+  ScrollView,
+  StyleSheet,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import React, { useLayoutEffect } from "react";
 import { StatusBar } from "expo-status-bar";
+import CustomListItem from "../Components/CustomListItem";
+import { Avatar, Icon } from "react-native-elements";
+import { AntDesign, SimpleLineIcons } from "@expo/vector-icons";
+import { auth } from "../firebase";
 
-const HomeScreen = () => {
+const HomeScreen = ({ navigation }) => {
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      title: "Viper",
+      headerStyle: { backgroundColor: "#6534ec" },
+      headerTitleStyle: { color: "white" },
+      headerTint: { color: "white" },
+      headerLeft: () => (
+        <View style={{ marginLeft: 20 }}>
+          <TouchableOpacity activeOpacity={0.5}>
+            <Avatar
+              rounded
+              size={37}
+              source={{ uri: auth?.currentUser?.photoURL }}
+            />
+          </TouchableOpacity>
+        </View>
+      ),
+      headerRight: () => (
+        <View
+          style={{
+            marginRight: 20,
+            flexDirection: "row",
+            justifyContent: "space-between",
+            width: 80,
+          }}
+        >
+          <TouchableOpacity activeOpacity={0.5}>
+            <AntDesign name="camerao" size={24} color="black"></AntDesign>
+          </TouchableOpacity>
+          <TouchableOpacity activeOpacity={0.5}>
+            <SimpleLineIcons
+              name="pencil"
+              size={24}
+              color="black"
+            ></SimpleLineIcons>
+          </TouchableOpacity>
+        </View>
+      ),
+    });
+  }, []);
   return (
     <SafeAreaView>
       <ScrollView>
         <StatusBar style="light" />
-        <Text>HomeScreen</Text>
+        <CustomListItem />
       </ScrollView>
     </SafeAreaView>
   );
